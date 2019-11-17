2dfa6905482e7ec1655660df57acd53c9ea39e4a
Jamena McInteer <jamena@webdevabq.com>
2019-11-16 20:23:00 -0600

    added page header and subheader and fixed issue with user profile image

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const GlobalStyles = createGlobalStyle`
     background: url("/images/background-desktop.jpg") no-repeat center center fixed; 
     background-color: ${(props: IPropsTheme) => props.theme.pageBackgroundColor};
     background-size: cover;
+    color: ${(props: IPropsTheme) => props.theme.baseFontColor};
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
@@ -39,11 +40,30 @@ const AppBody = styled.div`
   color: ${(props: IPropsTheme) => props.theme.baseFontColor};
 `
 
+const AppHeader = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  margin: 0;
+`
+const AppHeaderText = styled.h1`
+  font-size: 3rem;
+  margin: 0;
+  padding: 3rem 0 1rem 0;
+`
+const AppHeaderSubtext = styled.h2`
+  font-size: 1.4rem;
+  font-weight: normal;
+  margin: 0;
+`
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <AppContainer>
         <GlobalStyles />
+        <AppHeader>
+          <AppHeaderText>#IoT</AppHeaderText>
+          <AppHeaderSubtext>Top 20 words in 100 recent tweets</AppHeaderSubtext>
+        </AppHeader>
         <AppBody>
           <MainLayout />
         </AppBody>
